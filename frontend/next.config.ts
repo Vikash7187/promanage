14c983b4f783bd03d3025df79a66dfe55dbb8d7b
Vikash <vikash@gmail.com>
2026-05-04 17:30:23 +0530

    Add CSP header in next.config.ts to allow Google Fonts and fix font loading error

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+  font-src 'self' https://fonts.gstatic.com;
+  img-src * data: blob:;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  connect-src *;
+  frame-src *;
+`;
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
